bf4a59bf0992fddd54ebf30cb4a99483fa3a5a12
cgh96 <tt878429@gmail.com>
2024-10-09 20:16:12 +0900

    feat : posts module을 분리 생성

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,26 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-interface IPost {
-  author: string;
-  title: string;
-  content: string;
-  likeCount: number;
-  commentCount: number;
-}
-
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
-  @Get('/post')
-  getPost(): IPost {
-    return {
-      author: 'newjeans_ofiicial',
-      title: '뉴진스 민지',
-      content: '민지가 민지',
-      likeCount: 100000,
-      commentCount: 33333,
-    };
-  }
 }
---
+++ src/posts/posts.controller.ts
@@ -0,0 +1,56 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { PostsService } from './posts.service';
+import { CreatePostDto } from './dto/create-post.dto';
+import { UpdatePostDto } from './dto/update-post.dto';
+
+interface IPost {
+  author: string;
+  title: string;
+  content: string;
+  likeCount: number;
+  commentCount: number;
+}
+
+@Controller('posts')
+export class PostsController {
+  constructor(private readonly postsService: PostsService) {}
+
+  @Post()
+  create(@Body() createPostDto: CreatePostDto) {
+    return this.postsService.create(createPostDto);
+  }
+
+  @Get()
+  getPost(): IPost {
+    return {
+      author: 'newjeans_ofiicial',
+      title: '뉴진스 민지',
+      content: '민지가 민지',
+      likeCount: 100000,
+      commentCount: 33333,
+    };
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.postsService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updatePostDto: UpdatePostDto) {
+    return this.postsService.update(+id, updatePostDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.postsService.remove(+id);
+  }
+}
