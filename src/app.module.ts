f2319403b8db77f8492f170c55330115450b84ef
cgh96 <tt878429@gmail.com>
2024-11-17 19:38:37 +0900

    chore: App Module에 typeORM 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,24 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostsModule } from './posts/posts.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [PostsModule],
+  imports: [
+    PostsModule,
+    TypeOrmModule.forRoot({
+      // 데이터베이스 타입
+      type: 'postgres',
+      host: process.env.HOST,
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'postgres',
+      entities: [],
+      // Nestjs에서 작성하는 TypeOrm코드와 DB의 싱크를 자동으로 맞출 것인지에 대한 여부 (DEV에서만 true)
+      synchronize: process.env.DB_TYPEORM_SYNC === 'true',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
